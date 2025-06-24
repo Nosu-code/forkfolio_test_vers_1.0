90ca35c2de4cb02d258b8c09ed6beef45a6c5a4d
Nosu-code <noellie.suos@outlook.fr>
2025-06-24 19:45:51 +0200

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "Noellie SUOS",
   profile: '../../public/PFP.jpg',
-  designation: "Junior Developer",
+  designation: "développeuse junior",
   description: "Je suis une étudiante en développement. Curieuse et investie dans le monde de la programmation, je cherche à developper des compétences en UX design. ",
   email: 'placeholdermail@mail.fr',
   phone: '+06000000000',
