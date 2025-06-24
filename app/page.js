eea10e14115b282fd8afc741dfe7dfc7c88e505a
Nosu-code <noellie.suos@outlook.fr>
2025-06-24 19:07:39 +0200

    Update page.js
    comment

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+//dans ce fichier d'initiation de page, chaque section va être ajouté sur la page
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
