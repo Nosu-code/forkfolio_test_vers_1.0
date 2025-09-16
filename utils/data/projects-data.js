6d78959481e2c68544c2cf844aeb69b220b551f2
Nosu-code <noellie.suos@outlook.fr>
2025-09-16 22:31:13 +0200

    Update projects-data.js
    change in projects info

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,39 +1,43 @@
 export const projectsData = [
     {
        id: 1,
-        name: 'GLPI L&aposA',
-        description: " Création d'une application de gestion de stock de bibliothèque. Création d'une interface graphique et de base de donnée pour gérer l'emprunt, le retour et les stocks des livres.",
-        tools: ['Windowform','C#'],
-        role: 'Developpeuse',
-        code: '',
-        demo: '',
+        name: "support d'analyse de conformité",
+        contexte: " Les Données Personnelles sont réglementées par les lois européennes, le RGPD. Avec l’augmentation des IA et de leur utilisation, se pose la question des données et de leur utilisation dans ce domaine. Depuis le 1er aout 2024, l’Union Européenne a publié l’IA ACT, un texte de loi pour encadrer l’usage des IA. Dans le Cadre de mon stage, j’ai étudié ; auprès de la DPO de l’académie de Lyon ; ce nouveau règlement.",
+        besoin: "Pour aider les professeurs souhaitant utiliser les IA, nous souhaitions créer un support d'aide à l'évaluation de la conformité d'une IA en concordance avec le RIA. Un support qui analysera l’utilisation, les données et le domaine d’étude de l’IA. ",
+        tools: ['textes RGPD','Google Colab','Albert'],        
+        realisation: "J’ai étudié les textes de lois pour calibrer des questions auxquels les utilisateurs devront répondre."+ 
+            "Les utilisateurs enregistrent dans un questionnaire détaillé des informations concernant leur IA tel que le but,"+
+       " les données récoltées pour son entrainement ainsi que leur utilisation. J’ai ensuite étudié des solutions possibles ;"+
+"nous souhaitions partir sur le même principe qu’une des IA utilisé au sein de l’organisme. "+
+          "  J’ai donc entrepris des recherches pour comprendre les IA et leur utilisation et ais proposé une version de prompt.",
+        bilan: "Bien que le projet n’ait pas pu aboutir à une production fonctionnelle, celui-ci m’a permis de développer des compétences sur les nouvelles technologies, les IA génératrice. J’ai aussi acquis connaissance sur des notions importantes tel que la gestion des données, ainsi que l’optimisation des récoltes de données mais aussi les risques de cybersécurité et des enjeux juridique associés à toutes ces notions. ",
     },
     {
         id: 2,
         name: 'GLPI',
-        description: " Création d'une application de gestion de stock de bibliothèque. Création d'une interface graphique et de base de donnée pour gérer l'emprunt, le retour et les stocks des livres.",
-        tools: ['Windowform','C#'],
-        role: 'Developpeuse',
-        code: '',
-        demo: '',
+        contexte: "Pour enrichir mes connaissances en première année de BTS SIO, nous avons entrepris l'installation de GLPI sur un système Linux Debian 12. L’objectif étant d'acquérir une compréhension approfondie d’un processus d'installation de logiciel de gestion de parc informatique au sein d'un environnement Debian.",
+        besoin: "Le logiciel GLPI très utilisé dans le domaine de la gestion de parc informatique, permet d’appréhender les gestions de tickets et d’un parc informatique. De plus, par ce projet se développe des compétences d’installation sur du Linux. ",
+        tools: ['ferme de serveur','Debian12'],        
+        realisation: 'Après l’installation des Machines virtuelle, nous avons installé les serveur apache et les extensions php, MySQL. Ensuite nous avons installé le GLPI et configuré les droits administrateur et la création de compte. Nous avons ensuite réalisé une base de donnés manuellement et tester les fonctions de ticketing.',
+        bilan:'Ce projet m’a permis de développer des connaissances d’installation et une plus ample compréhension des systèmes linux, des machines virtuelles et des dépendances réseau. L’utilisation de GLPI fut un expérience réelle de gestion de ticket et d’insertion dans un environnement de travail. ',
     },
     {
         id: 3,
-        name: 'Portefolio',
-        description:"Création d'un portefolio professionnel",
-        tools: ['HTLM','Node','JS','CSS','Git'],
-        role: '',
-        code: '',
-        demo: '',
+        name: "",
+         contexte: "",
+         besoin: " ",
+         tools: [''],        
+         realisation: '',
+         bilan:'',
     },
     {
-        id: 4, 
-        name: '',
-        description: "",
-        tools: [],
-        role: '',
-        code: '',
-        demo: '',
+         id: 4,
+         name: "",
+         contexte: "",
+         besoin: " ",
+         tools: [''],        
+         realisation: '',
+         bilan:'',
         
     }
 ];
@@ -52,13 +56,14 @@ export const projectsData = [
 //     demo: '',
 
 // id: 1,
-//         name: '',
-//        context: 'Siteweb',
-//        besoin: " Création d'un siteweb en HTML. Initiation au CSS et au JS et au Framework tel que Bootstrap",
-//        Env: ['HTML','CSS','JS','Bootstrap'],
+//        id: 1,
+//        name: "",
+//        contexte: "",
+//        besoin: " ",
+//        tools: ['textes RGPD','Google Colab','Albert'],        
 //        realisation: '',
-//        bilan: '',
-//        comp: '',     
+//        bilan:'',
+    
 // },
 
 
