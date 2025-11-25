6a4241b42c544ae7fae4af4a9057986e054ed16f
Nosu-code <noellie.suos@outlook.fr>
2025-11-25 19:49:21 +0100

    Update projects-data.js
    text test

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -5,9 +5,9 @@ export const projectsData = [
         contexte: " Les Données Personnelles sont réglementées par les lois européennes, le RGPD. Avec l’augmentation des IA et de leur utilisation, se pose la question des données et de leur utilisation dans ce domaine. Depuis le 1er aout 2024, l’Union Européenne a publié l’IA ACT, un texte de loi pour encadrer l’usage des IA. Dans le Cadre de mon stage, j’ai étudié ; auprès de la DPO de l’académie de Lyon ; ce nouveau règlement.",
         besoin: "Pour aider les professeurs souhaitant utiliser les IA, nous souhaitions créer un support d'aide à l'évaluation de la conformité d'une IA en concordance avec le RIA. Un support qui analysera l’utilisation, les données et le domaine d’étude de l’IA. ",
         tools: ['textes RGPD','Google Colab','Albert'],        
-        realisation: "J’ai étudié les textes de lois pour calibrer des questions auxquels les utilisateurs devront répondre."+ 
-            "Les utilisateurs enregistrent dans un questionnaire détaillé des informations concernant leur IA tel que le but,"+
-       " les données récoltées pour son entrainement ainsi que leur utilisation. J’ai ensuite étudié des solutions possibles ;"+
+        realisation: "1.J’ai étudié les textes de lois pour calibrer des questions auxquels les utilisateurs devront répondre."+ 
+            +"<br>"+"2.Les utilisateurs enregistrent dans un questionnaire détaillé des informations concernant leur IA tel que le but,"+
+       " les données récoltées pour son entrainement ainsi que leur utilisation."+<br>+"3.J’ai ensuite étudié des solutions possibles ;"+
 "nous souhaitions partir sur le même principe qu’une des IA utilisé au sein de l’organisme. "+
           "  J’ai donc entrepris des recherches pour comprendre les IA et leur utilisation et ais proposé une version de prompt.",
         bilan: "Bien que le projet n’ait pas pu aboutir à une production fonctionnelle, celui-ci m’a permis de développer des compétences sur les nouvelles technologies, les IA génératrices. J’ai aussi acquis des connaissances sur des notions importantes telles que la gestion des données, ainsi que l’optimisation des récoltes de données mais aussi les risques de cybersécurité et des enjeux juridique associés à toutes ces notions. ",
