e3bd9fdee9709ad3775c972975a0040a4713e1ae
Nosu-code <noellie.suos@outlook.fr>
2026-04-19 22:58:58 +0200

    Update navbar.jsx

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -32,6 +32,10 @@ function Navbar() {
           <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/blog"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">VEILLE TECHNOLOGIQUE</div></Link>
           </li>          
+           <li>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/tableau_E7.pdf" target="_blank"
+                rel="noopener noreferrer" ><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">VEILLE TECHNOLOGIQUE</div></Link>
+          </li>    
         </ul>
       </div>
     </nav>
